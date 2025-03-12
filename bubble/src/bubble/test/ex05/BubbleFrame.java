4c35f3d66e102a3c165a5a3a0df4e68622a11be8
MooMoongg <alchemist72727@gmail.com>
2025-03-12 14:45:16 +0900

    charactoer jump control 구현완료

---
+++ bubble/src/bubble/test/ex05/BubbleFrame.java
@@ -0,0 +1,83 @@
+package bubble.test.ex05;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame {
+
+	private JLabel backgroudMap;
+	private Player player;
+
+	public BubbleFrame() {
+		initObject();
+		initSetting();
+		initListener();
+		setVisible(true);
+	}
+
+	private void initObject() {
+		backgroudMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+		setContentPane(backgroudMap);
+		player = new Player();
+		add(player);
+	}
+
+	private void initSetting() {
+		setSize(1000, 640);
+		setLayout(null);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// System.out.println(e.getKeyCode());
+
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					if (!player.isLeft()) {
+						player.left();
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if (!player.isRight()) {
+						player.right();
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if(!player.isUp() && !player.isDown()) {
+						player.up();
+					}
+					player.up();
+					break;
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				}
+			}
+
+		});
+	}
+
+	public static void main(String[] args) {
+		new BubbleFrame();
+
+	}
+
+}
