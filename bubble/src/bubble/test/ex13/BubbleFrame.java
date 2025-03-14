68ddd7943751cdf8ab7a51d317637d419828b2cd
MooMoongg <alchemist72727@gmail.com>
2025-03-14 19:57:19 +0900

    feat: repaint

---
+++ bubble/src/bubble/test/ex13/BubbleFrame.java
@@ -0,0 +1,91 @@
+package bubble.test.ex13;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BubbleFrame extends JFrame {
+
+	private BubbleFrame mContext = this;
+	private JLabel backgroudMap;
+	private Player player;
+
+	public BubbleFrame() {
+		initObject();
+		initSetting();
+		initListener();
+		setVisible(true);
+	}
+
+	private void initObject() {
+		backgroudMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+		setContentPane(backgroudMap);
+		player = new Player();
+		add(player);
+	}
+
+	private void initSetting() {
+		setSize(1000, 640);
+		setLayout(null);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// System.out.println(e.getKeyCode());
+
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					if (!player.isLeft() && !player.isLeftWallCrash()) {
+						player.left();
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if (!player.isRight() && !player.isRightWallCrash()) {
+						player.right();
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if (!player.isUp() && !player.isDown()) {
+						player.up();
+					}
+					break;
+				case KeyEvent.VK_SPACE:
+					Bubble bubble = new Bubble(mContext);
+					add(bubble);
+					break;
+				}
+			}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			player.setLeft(false);
+			break;
+		case KeyEvent.VK_RIGHT:
+			player.setRight(false);
+			break;
+		}
+	}
+
+	});}
+
+	public static void main(String[] args) {
+		new BubbleFrame();
+
+	}
+
+}
