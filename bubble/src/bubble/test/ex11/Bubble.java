d8fd3862bfff6d0ca26b5a5ae2d853b20b411c7a
MooMoongg <alchemist72727@gmail.com>
2025-03-14 15:10:51 +0900

    feat: 물방울 발사

--- bubble/src/bubble/test/ex11/Bubble.java
+++ bubble/src/bubble/test/ex11/Bubble.java
@@ -33,6 +33,7 @@ public class Bubble extends JLabel implements Moveable{
 		this.player = player;
 		initObject();
 		initSetting();
+		initThread();
 	}
 	
 	private void initObject() {
@@ -55,18 +56,61 @@ public class Bubble extends JLabel implements Moveable{
 		state = 0;
 	}
 
+	private void initThread() {
+		// 버블은 스레드가 하나 필요
+		new Thread(()->{
+			if(player.getPlayerWay() == PlayerWay.LEFT) {
+				left();
+			}else {
+				right();
+			}
+		}).start();
+	}
+	
 	@Override
 	public void left() {
-
+		left = true;
+		for(int i = 0; i<400; i++) {
+			x--;
+			setLocation(x, y);
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
 	}
 
 	@Override
 	public void right() {
-	
+		right = true;
+		for(int i = 0; i<400; i++) {
+			x++;
+			setLocation(x, y);
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
 	}
 
 	@Override
 	public void up() {
-	
+		up = true;
+		while(up) {
+			y--;
+			setLocation(x, y);
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
